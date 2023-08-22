09d3b88055055622764c2c980d23c30ad05b27e1
brokentractor <michael@brokentractor.com>
2023-08-22 11:18:05 -0500

    undo core dev

--- assets/js/theme/product.js
+++ assets/js/theme/product.js
@@ -81,7 +81,7 @@ export default class Product extends PageManager {
             } else if (isNaN(startprice) == false) {
               newcprice = newcprice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
               startprice = startprice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-              $('.productView-price').append('<div>Total After Core Return'$'+ newcprice+'</div>');
+              $('.price.price--withoutTax').html('$'+ newcprice);
               $('.productView-price').append('<div class="corecharge">Refundable Core Deposit: $'+ coreprice+'</div>');
               $('.productView-price').append('<hr class="totalLine"><div class="totalwithcore">Total: $'+ startprice+'</div>');
 
